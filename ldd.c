d4f6634e6310f4b46c938a21fe16a99e2b761759
Kirubakaran <78558971+kirubakaran4@users.noreply.github.com>
2025-04-10 10:33:31 +0530

    Update ldd.c

--- ldd.c
+++ ldd.c
@@ -1,6 +1,20 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("KIRUBA");
+MODULE_DESCRIPTION("LEARNING DEVICE DRIVERS");
+
+static int learn_init(void)
+{
+printk("Hello Kernel \n");
+return 0;
+}
+
+static void learn_exit(void)
+{
+printk("Exiting from Kernel \n");
+}
 
 module_init(learn_init);
 module_exit(leran_exit);
